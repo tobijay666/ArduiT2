eabf157da499b2294ee1be2c512996fe7e7f0602
Menaka Anton <menakaantonjayakody@gmail.com>
2022-05-03 21:34:25 +0530

    motion sen distancce taken

--- SetData.php
+++ SetData.php
@@ -9,21 +9,21 @@
 
     $distance = (int) $stat;
 
-    if ($distance <= 100){
+    if ($distance <= 10){
       $state = "Triggered";
     }
 
-    else if ($distance > 100){
+    else if ($distance > 10){
       $state = "Inactive";
     }
 
-    $sql2 = "UPDATE motionsensor SET Distance = $distance"; 
+    $sql2 = "UPDATE motionsensor SET Distance = $distance where ID = 1"; 
     $sql = "UPDATE statusled SET Stat='$state' WHERE ID='$id' ";
     
     $squ = mysqli_query($cont,$sql);
     //$data = mysqli_fetch_assoc($squ);
     Database::disconnect();
     
-    echo $state;
+    //echo $state;
   }
 ?>
